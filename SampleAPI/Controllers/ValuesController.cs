3654e7a19202e0e20855d166ea42c9ba80270d0d
My Name <86416827+shanchamara@users.noreply.github.com>
2024-11-12 07:11:15 +0530

    first commi1t4

---
+++ SampleAPI/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("test-one")]
+        public IActionResult TestOne()
+        {
+            return Ok("TestOne response ok ");
+        }
+    }
+
+}
